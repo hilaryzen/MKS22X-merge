bca6a31d6e085afb00e14a28093328048ecf63a1
Hilary Zen <hzen00@stuy.edu>
2019-03-15 10:51:24 -0400

    Worked on splitting data array

--- Merge.java
+++ Merge.java
@@ -1,5 +1,18 @@
 public class Merge {
   public static void mergesort(int[] data) {
-    
+
+  }
+
+  public static void mergeH(int[] data) {
+    //Split data into two halves
+    int mid = data.length / 2;
+    int[] first = new int[mid];
+    int[] second = new int[data.length - mid];
+    for (int i = 0; i < mid; i++) {
+      first[i] = data[i];
+    }
+    for (int j = mid; j < data.length; j++) {
+      second[j - mid] = data[j];
+    }
   }
 }
